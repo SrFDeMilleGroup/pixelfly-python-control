3af570fcc99b9a2b8dddf9a07acab1c976841490
qw372 <qian.wang.qw95@yale.edu>
2020-12-08 18:12:03 -0500

    camera configs

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import h5py
 import time
 import logging
 import traceback
-import threading
 import numpy as np
 import configparser
 import PyQt5
@@ -12,6 +11,7 @@ import pyqtgraph as pg
 import PyQt5.QtGui as QtGui
 import PyQt5.QtWidgets as qt
 from collections import deque
+import pco
 import qdarkstyle # see https://github.com/ColinDuquesnoy/QDarkStyleSheet
 
 # use "cam.sdk.get_camera_description()" to check camera specs:
@@ -63,19 +63,23 @@ import qdarkstyle # see https://github.com/ColinDuquesnoy/QDarkStyleSheet
 # dwGeneralCapsDESC3: 0
 # dwGeneralCapsDESC4: 0
 
-sensor_size_options = ["standard (1392*1040)", "extended (800*600)"]
-sensor_size_default = sensor_size_options[0]
-clock_rate_options = ["12 MHz", "24 MHz"]
-clock_rate_default = clock_rate_options[0]
-conv_factor_options = ["1", "1.5"]
-conv_factor_default = conv_factor_options[0]
-trigger_mode_options = ["auto sequence", "software trigger", "external/software trigger"]
-trigger_mode_default = trigger_mode_options[2]
+sensor_format_options = {"standard (1392*1040)": "standard", "extended (800*600)": "extended"}
+sensor_format_default = list(sensor_format_options.keys())[0]
+clock_rate_options = {"12 MHz": 12000000, "24 MHz": 24000000}
+clock_rate_default = list(clock_rate_options.keys())[0]
+conv_factor_options = {"1": 100, "1.5": 150} # 100/gain, or electrons/count*100
+conv_factor_default = list(conv_factor_options.keys())[0]
+trigger_mode_options = {"auto sequence": "auto sequence", "software trigger": "software trigger",
+                        "external/software trigger": "external exposure start & software trigger"}
+trigger_mode_default = list(trigger_mode_options.keys())[2]
 trigger_source_options = ["software", "external TTL"]
 trigger_source_default = trigger_source_options[1]
-expo_unit_options = ["ms", "us", "ns"]
-expo_unit_default = expo_unit_options[0]
+expo_unit_options = {"ms": 1e-3, "us": 1e-6} # unit "ns" is not implemented bacause the min expo time is 1000 ns = 1 us
+expo_unit_default = list(expo_unit_options.keys())[0]
 expo_time_default = "10" # in the unit of expo_unit_default
+expo_min = 1000e-9 # Min Expos DESC in ns
+expo_max = 60000e-3 # Max Expos DESC in ms
+expo_decimals = 6 # converted from Min Expos Step DESC in ns
 binning_max = 4 # same for both horizontal and vertical
 binning_default = (1, 1) # (horizontal, vertical)
 gaussian_fit_default = False
@@ -105,34 +109,73 @@ def Scrollarea(label="", type="grid"):
 
 class pixelfly:
     def __init__(self, parent):
-        self.parent=parent
+        self.parent = parent
 
-    def load_configs(self):
-        pass
+        # try:
+        #     self.cam = pco.Camera(interface='USB 2.0')
+        # except Exception as err:
+        #     logging.error(traceback.format_exc())
+        #     logging.error("Can't open camera")
+        #     return
+        #
+        # self.set_sensor_format(sensor_format_default)
+        # self.set_clock_rate(clock_rate_default)
+        # self.set_conv_factor(conv_factor_default)
+        # self.set_trigger_mode(trigger_mode_default)
+        # self.trigger_source = trigger_source_default
+        # self.set_expo_time(expo_time_default, expo_unit_default)
+        # self.set_binning(binning_default[0], binning_default[1])
+        # self.gaussian_fit = gaussian_fit_default
 
-    def set_sensor_size(self, arg):
-        pass
-
-    def set_trigger_mode(self, arg):
-        pass
-
-    def set_trigger_source(self):
-        pass
+    def set_sensor_format(self, arg):
+        self.cam.sdk.set_sensor_format(sensor_format_options[arg])
+        self.cam.sdk.arm_camera()
 
     def set_clock_rate(self, arg):
-        pass
+        self.cam.configuration = {"pixel rate": clock_rate_options[arg]}
 
     def set_conv_factor(self, arg):
-        pass
+        self.cam.sdk.set_conversion_factor(conv_factor_options[arg])
+        self.cam.sdk.arm_camera()
 
-    def set_expo(self, time, unit):
+    def set_trigger_mode(self, arg):
+        self.cam.configuration = {"trigger": trigger_mode_options[arg]}
+
+    def set_trigger_source(self, text, checked):
+        if checked:
+            self.trigger_source = text
+
+    def set_expo_time(self, time, unit):
         # exposure time sanity check
-        pass
+        try:
+            expo_time = float(time)*expo_unit_options[unit]
+        except ValueError as err:
+            logging.warning(traceback.format_exc())
+            logging.warning("Exposure time invalid!")
+            return
+
+        expo_time_round = round(expo_time, expo_decimals)
+        if expo_time_round < expo_min:
+            expo_time_round = expo_min
+        elif expo_time_round > expo_max:
+            expo_time_round = expo_max
+        self.cam.configuration = {'exposure time': expo_time_round}
+
+        d = int(expo_decimals+np.log10(expo_unit_options[unit]))
+        if d:
+            t = round(expo_time_round/expo_unit_options[unit], d)
+            t = f"{t}"
+        else:
+            t = "{:d}".format(round(expo_time_round/expo_unit_options[unit]))
+        return t
 
     def set_binning(self, bin_h, bin_v):
-        pass
+        self.cam.configuration = {'binning': (int(bin_h), int(bin_v))}
 
     def set_gauss_fit(self, state):
+        self.gaussian_fit = state
+
+    def load_configs(self):
         pass
 
 
@@ -143,37 +186,55 @@ class Controls(qt.QWidget):
         self.frame = qt.QVBoxLayout()
         self.setLayout(self.frame)
 
+        self.place_record_elements()
         self.place_control_elements()
         self.place_indicator_elements()
 
-    def place_control_elements(self):
-        outer_box, control_frame = Scrollarea(label="Controls", type="form")
+    def place_record_elements(self):
+        outer_box, record_frame = Scrollarea(label="Recording", type="grid")
         self.frame.addWidget(outer_box)
 
-        self.sensor_size_cb = qt.QComboBox()
-        for i in range(len(sensor_size_options)):
-            self.sensor_size_cb.addItem(sensor_size_options[i])
-        self.sensor_size_cb.setCurrentText(sensor_size_default)
-        self.sensor_size_cb.activated[str].connect(lambda val: self.parent.device.set_sensor_size(val))
-        control_frame.addRow("Sensor size:", self.sensor_size_cb)
+        self.record_bt = qt.QPushButton("Record")
+        self.record_bt.clicked[bool].connect(lambda val: self.start())
+        record_frame.addWidget(self.record_bt, 0, 0)
+
+        self.scan_bt = qt.QPushButton("Scan")
+        self.scan_bt.clicked[bool].connect(lambda val: self.scan())
+        record_frame.addWidget(self.scan_bt, 0, 1)
+
+        self.stop_bt = qt.QPushButton("Stop")
+        self.stop_bt.clicked[bool].connect(lambda val: self.stop())
+        record_frame.addWidget(self.stop_bt, 0, 2)
+        self.stop_bt.setEnabled(False)
+
+    def place_control_elements(self):
+        self.control_elem_box, control_frame = Scrollarea(label="Controls", type="form")
+        self.frame.addWidget(self.control_elem_box)
+
+        self.sensor_format_cb = qt.QComboBox()
+        for i in range(len(sensor_format_options)):
+            self.sensor_format_cb.addItem(list(sensor_format_options.keys())[i])
+        self.sensor_format_cb.setCurrentText(sensor_format_default)
+        self.sensor_format_cb.activated[str].connect(lambda val: self.parent.device.set_sensor_format(val))
+        control_frame.addRow("Sensor size:", self.sensor_format_cb)
 
         self.clock_rate_cb = qt.QComboBox()
         for i in range(len(clock_rate_options)):
-            self.clock_rate_cb.addItem(clock_rate_options[i])
+            self.clock_rate_cb.addItem(list(clock_rate_options.keys())[i])
         self.clock_rate_cb.setCurrentText(clock_rate_default)
         self.clock_rate_cb.activated[str].connect(lambda val: self.parent.device.set_clock_rate(val))
         control_frame.addRow("Clock rate:", self.clock_rate_cb)
 
         self.conv_factor_cb = qt.QComboBox()
         for i in range(len(conv_factor_options)):
-            self.conv_factor_cb.addItem(conv_factor_options[i])
+            self.conv_factor_cb.addItem(list(conv_factor_options.keys())[i])
         self.conv_factor_cb.setCurrentText(conv_factor_default)
         self.conv_factor_cb.activated[str].connect(lambda val: self.parent.device.set_conv_factor(val))
         control_frame.addRow("Conversion factor:", self.conv_factor_cb)
 
         self.trigger_mode_cb = qt.QComboBox()
         for i in range(len(trigger_mode_options)):
-            self.trigger_mode_cb.addItem(trigger_mode_options[i])
+            self.trigger_mode_cb.addItem(list(trigger_mode_options.keys())[i])
         self.trigger_mode_cb.setCurrentText(trigger_mode_default)
         self.trigger_mode_cb.activated[str].connect(lambda val: self.parent.device.set_trigger_mode(val))
         control_frame.addRow("Trigger mode:", self.trigger_mode_cb)
@@ -187,7 +248,7 @@ class Controls(qt.QWidget):
         for i in range(len(trigger_source_options)):
             trig_source_rb = qt.QRadioButton(trigger_source_options[i])
             trig_source_rb.setChecked(True if trigger_source_default == trigger_source_options[i] else False)
-            trig_source_rb.toggled.connect(self.parent.device.set_trigger_source)
+            trig_source_rb.toggled[bool].connect(lambda val, rb=trig_source_rb: self.parent.device.set_trigger_source(rb.text(), val))
             self.trig_source_rblist.append(trig_source_rb)
             trig_bg.addButton(trig_source_rb)
             trig_layout.addWidget(trig_source_rb)
@@ -197,11 +258,11 @@ class Controls(qt.QWidget):
         self.expo_le.setText(expo_time_default)
         self.expo_unit_cb = qt.QComboBox()
         for i in range(len(expo_unit_options)):
-            self.expo_unit_cb.addItem(expo_unit_options[i])
+            self.expo_unit_cb.addItem(list(expo_unit_options.keys())[i])
         self.expo_unit_cb.setCurrentText(expo_unit_default)
         self.expo_le.editingFinished.connect(lambda le=self.expo_le, cb=self.expo_unit_cb:
-                                            self.parent.device.set_expo(le.text(), cb.currentText()))
-        self.expo_unit_cb.activated[str].connect(lambda val, le=self.expo_le: self.parent.device.set_expo(le.text(), val))
+                                            self.set_expo_time(le.text(), cb.currentText()))
+        self.expo_unit_cb.activated[str].connect(lambda val, le=self.expo_le: self.set_expo_time(le.text(), val))
         expo_box = qt.QWidget()
         expo_layout = qt.QHBoxLayout()
         expo_layout.setContentsMargins(0,0,0,0)
@@ -234,11 +295,11 @@ class Controls(qt.QWidget):
         control_frame.addRow("2D gaussian fit:", self.gauss_fit_chb)
 
         self.load_settings_bt = qt.QPushButton("load settings")
-        self.load_settings_bt.clicked[bool].connect(self.load_settings)
+        self.load_settings_bt.clicked[bool].connect(lambda val: self.load_settings())
         control_frame.addRow("Load settings:", self.load_settings_bt)
 
         self.save_settings_bt = qt.QPushButton("save settings")
-        self.save_settings_bt.clicked[bool].connect(self.save_settings)
+        self.save_settings_bt.clicked[bool].connect(lambda val: self.save_settings())
         control_frame.addRow("Save settings:", self.save_settings_bt)
 
     def place_indicator_elements(self):
@@ -306,6 +367,28 @@ class Controls(qt.QWidget):
         self.offset.setStyleSheet("background-color: gray;")
         indicator_frame.addRow("2D gaussian fit (offset):", self.offset)
 
+    def start(self):
+        self.record_bt.setEnabled(False)
+        self.scan_bt.setEnabled(False)
+        self.stop_bt.setEnabled(True)
+        self.control_elem_box.setEnabled(False)
+
+    def scan(self):
+        self.scan_bt.setEnabled(False)
+        self.record_bt.setEnabled(False)
+        self.stop_bt.setEnabled(True)
+        self.control_elem_box.setEnabled(False)
+
+    def stop(self):
+        self.stop_bt.setEnabled(False)
+        self.record_bt.setEnabled(True)
+        self.scan_bt.setEnabled(True)
+        self.control_elem_box.setEnabled(True)
+
+    def set_expo_time(self, time, unit):
+        t = self.parent.device.set_expo_time(time, unit)
+        self.expo_le.setText(t)
+
     def load_settings(self):
         pass
 
@@ -344,4 +427,5 @@ if __name__ == '__main__':
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     main_window = CameraGUI()
     app.exec_()
+    main_window.device.cam.close()
     sys.exit(0)
