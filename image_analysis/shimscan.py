f28e9fbc91b03acff68d78dafaea55e6f181df74
qian372 <qian.wang.qw95@yale.edu>
2021-05-28 15:24:03 -0400

    include image analysis files

---
+++ image_analysis/shimscan.py
@@ -0,0 +1,92 @@
+import numpy as np
+from scipy import optimize
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import h5py
+
+def gaussian(amp, x_mean, y_mean, x_width, y_width, offset):
+    x_width = float(x_width)
+    y_width = float(y_width)
+
+    return lambda x, y: amp*np.exp(-0.5*((x-x_mean)/x_width)**2-0.5*((y-y_mean)/y_width)**2) + offset
+
+# return a 2D gaussian fit
+# generally a 2D gaussian fit can have 7 params, 6 of them are implemented here (the excluded one is an angle)
+# codes adapted from https://scipy-cookbook.readthedocs.io/items/FittingData.html
+def gaussianfit(data, roi, showimg=False):
+    # calculate moments for initial guess
+    data = data[roi["xmin"]:roi["xmax"], roi["ymin"]:roi["ymax"]]
+    if showimg:
+        plt.imshow(data)
+        plt.show()
+
+    total = np.sum(data)
+    X, Y = np.indices(data.shape)
+    x_mean = np.sum(X*data)/total
+    y_mean = np.sum(Y*data)/total
+    col = data[:, int(y_mean)]
+    x_width = np.sqrt(np.abs((np.arange(col.size)-x_mean)**2*col).sum()/col.sum())
+    row = data[int(x_mean), :]
+    y_width = np.sqrt(np.abs((np.arange(row.size)-y_mean)**2*row).sum()/row.sum())
+    offset = (data[0, :].sum()+data[-1, :].sum()+data[:, 0].sum()+data[:, -1].sum())/np.sum(data.shape)/2
+    amp = data.max() - offset
+
+    # use optimize function to obtain 2D gaussian fit
+    errorfunction = lambda p: np.ravel(gaussian(*p)(*np.indices(data.shape))-data)
+    p, success = optimize.leastsq(errorfunction, (amp, x_mean, y_mean, x_width, y_width, offset))
+
+    p_dict = {}
+    p_dict["x_mean"] = p[1]
+    p_dict["y_mean"] = p[2]
+    p_dict["x_width"] = p[3]
+    p_dict["y_width"] = p[4]
+    p_dict["amp"] = p[0]
+    p_dict["offset"] = p[5]
+
+    return p_dict
+
+filepath = "C:/Users/dur!p5/github/pixelfly-python-control/saved_images/"
+filename = "images_20210526.hdf"
+groupname = "dcfluormot_20210526_170235"
+pixeltomm = 0.1
+
+with h5py.File(filepath+filename, "r") as f:
+    group = f[groupname]
+    scanparam = np.array([])
+    axialwidth = np.array([])
+    axialwidth_err = np.array([])
+    radialwidth = np.array([])
+    radialwidth_err = np.array([])
+    scanparam_name = list(group.keys())[0].split("_")[0]
+
+    for subg in group.keys():
+        image_list = group[subg]
+        x_width_list = np.array([])
+        y_width_list = np.array([])
+        for img in image_list.keys():
+            img_data = group[subg][img]
+            roi = {"xmin":20, "xmax":260, "ymin":20, "ymax":200}
+            fitresult = gaussianfit(img_data, roi)
+            new_roi = {}
+            new_roi["xmin"] = int(np.maximum(roi["xmin"]+fitresult["x_mean"]-3*fitresult["x_width"], 0))
+            new_roi["xmax"] = int(np.minimum(roi["xmin"]+fitresult["x_mean"]+3*fitresult["x_width"], img_data.shape[0]))
+            new_roi["ymin"] = int(np.maximum(roi["ymin"]+fitresult["y_mean"]-3*fitresult["y_width"], 0))
+            new_roi["ymax"] = int(np.minimum(roi["ymin"]+fitresult["y_mean"]+3*fitresult["y_width"], img_data.shape[1]))
+            fitresult = gaussianfit(img_data, new_roi, showimg=False)
+            # print(fitresult)
+            x_width_list =np.append(x_width_list, fitresult["x_width"]*pixeltomm)
+            y_width_list =np.append(y_width_list, fitresult["y_width"]*pixeltomm)
+        axialwidth = np.append(axialwidth, np.mean(y_width_list))
+        axialwidth_err = np.append(axialwidth_err, np.std(y_width_list)/np.sqrt(len(y_width_list)))
+        radialwidth = np.append(radialwidth, np.mean(x_width_list))
+        radialwidth_err = np.append(radialwidth_err, np.std(x_width_list)/np.sqrt(len(x_width_list)))
+        scanparam = np.append(scanparam, float(subg.split("_")[-1]))
+
+mpl.style.use("seaborn")
+plt.errorbar(scanparam, radialwidth, yerr=radialwidth_err, marker='o', mfc='C1', markeredgewidth=0.8, markeredgecolor='k', ecolor='C1', linestyle='', label="radial rms radius")
+plt.errorbar(scanparam, axialwidth, yerr=axialwidth_err, marker='o', mfc='C2', markeredgewidth=0.8, markeredgecolor='k', ecolor='C2', linestyle='', label="axial rms radius")
+
+plt.xlabel(scanparam_name)
+plt.ylabel("cloud rms radius [mm]")
+plt.legend()
+plt.show()
---
+++ image_analysis/tof.py
@@ -0,0 +1,153 @@
+import numpy as np
+from scipy import optimize
+from scipy import stats
+import uncertainties.unumpy as unp
+import uncertainties as unc
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import h5py
+
+def gaussian(amp, x_mean, y_mean, x_width, y_width, offset):
+    x_width = float(x_width)
+    y_width = float(y_width)
+
+    return lambda x, y: amp*np.exp(-0.5*((x-x_mean)/x_width)**2-0.5*((y-y_mean)/y_width)**2) + offset
+
+# return a 2D gaussian fit
+# generally a 2D gaussian fit can have 7 params, 6 of them are implemented here (the excluded one is an angle)
+# codes adapted from https://scipy-cookbook.readthedocs.io/items/FittingData.html
+def gaussianfit(data, roi, showimg=False):
+    # calculate moments for initial guess
+    data = data[roi["xmin"]:roi["xmax"], roi["ymin"]:roi["ymax"]]
+    if showimg:
+        plt.imshow(data)
+        plt.show()
+
+    total = np.sum(data)
+    X, Y = np.indices(data.shape)
+    x_mean = np.sum(X*data)/total
+    y_mean = np.sum(Y*data)/total
+    col = data[:, int(y_mean)]
+    x_width = np.sqrt(np.abs((np.arange(col.size)-x_mean)**2*col).sum()/col.sum())
+    row = data[int(x_mean), :]
+    y_width = np.sqrt(np.abs((np.arange(row.size)-y_mean)**2*row).sum()/row.sum())
+    offset = (data[0, :].sum()+data[-1, :].sum()+data[:, 0].sum()+data[:, -1].sum())/np.sum(data.shape)/2
+    amp = data.max() - offset
+
+    # use optimize function to obtain 2D gaussian fit
+    errorfunction = lambda p: np.ravel(gaussian(*p)(*np.indices(data.shape))-data)
+    p, success = optimize.leastsq(errorfunction, (amp, x_mean, y_mean, x_width, y_width, offset))
+
+    p_dict = {}
+    p_dict["x_mean"] = p[1]
+    p_dict["y_mean"] = p[2]
+    p_dict["x_width"] = p[3]
+    p_dict["y_width"] = p[4]
+    p_dict["amp"] = p[0]
+    p_dict["offset"] = p[5]
+
+    return p_dict
+
+def linear(x, slope, offset):
+    return slope*x+offset
+
+def linearfit(x, y, yerr):
+    ind = np.argsort(x)
+    x_sorted = x[ind]
+    y_sorted = y[ind]
+    slope = (y_sorted[-1]-y_sorted[0])/(x_sorted[-1]-x_sorted[0])
+    offset = (y_sorted[0]*x_sorted[-1]-y_sorted[-1]*x_sorted[0])/(x_sorted[-1]-x_sorted[0])
+
+    popt, pcov = optimize.curve_fit(linear, x, y, p0=(slope, offset), sigma=yerr)
+
+    return (popt, pcov)
+
+class tofanalysis:
+    def __init__(self, fname, gname):
+        param = {"pixeltomm": 0.1, "kB": 1.38064852e-23, "m": 87*1.66053873e-27, "confidence_band": 0.95}
+
+        time_sq, axial_width_sq, axial_width_sq_err, radial_width_sq, radial_width_sq_err = self.readhdf(fname, gname, param)
+        mpl.style.use("seaborn")
+        self.fig, self.ax = plt.subplots()
+        self.plot(time_sq, radial_width_sq, radial_width_sq_err, type="radial", param=param)
+        self.plot(time_sq, axial_width_sq, axial_width_sq_err, type="axial", param=param)
+        self.ax.set_xlabel("time of flight$^2$ [ms$^2$]")
+        self.ax.set_ylabel("cloud rms radius$^2$ [mm$^2$]")
+        self.ax.legend()
+        plt.show()
+
+    def readhdf(self, fname, gname, param):
+        with h5py.File(fname, "r") as f:
+            group = f[gname]
+            time_sq = np.array([])
+            axial_width_sq = np.array([])
+            axial_width_sq_err = np.array([])
+            radial_width_sq = np.array([])
+            radial_width_sq_err = np.array([])
+
+            for subg in group.keys():
+                image_list = group[subg]
+                x_width_sq = np.array([])
+                y_width_sq = np.array([])
+                for img in image_list.keys():
+                    img_data = group[subg][img]
+                    roi = {"xmin":20, "xmax":260, "ymin":20, "ymax":200} # choose a braod roi for the first fit trial
+                    fitresult = gaussianfit(img_data, roi)
+                    new_roi = {} # calculate a new roi based on the first fit result (use +/-3sigma region)
+                    new_roi["xmin"] = int(np.maximum(roi["xmin"]+fitresult["x_mean"]-3*fitresult["x_width"], 0))
+                    new_roi["xmax"] = int(np.minimum(roi["xmin"]+fitresult["x_mean"]+3*fitresult["x_width"], img_data.shape[0]))
+                    new_roi["ymin"] = int(np.maximum(roi["ymin"]+fitresult["y_mean"]-3*fitresult["y_width"], 0))
+                    new_roi["ymax"] = int(np.minimum(roi["ymin"]+fitresult["y_mean"]+3*fitresult["y_width"], img_data.shape[1]))
+                    fitresult = gaussianfit(img_data, new_roi, showimg=False) # make a second fit using the new roi
+                    x_width_sq =np.append(x_width_sq, (fitresult["x_width"]*param["pixeltomm"])**2)
+                    y_width_sq =np.append(y_width_sq, (fitresult["y_width"]*param["pixeltomm"])**2)
+                axial_width_sq = np.append(axial_width_sq, np.mean(y_width_sq))
+                axial_width_sq_err = np.append(axial_width_sq_err, np.std(y_width_sq)/np.sqrt(len(y_width_sq)))
+                radial_width_sq = np.append(radial_width_sq, np.mean(x_width_sq))
+                radial_width_sq_err = np.append(radial_width_sq_err, np.std(x_width_sq)/np.sqrt(len(x_width_sq)))
+                time_sq = np.append(time_sq, (float(subg.split("_")[-1])/1e6)**2) # convert ns to ms
+
+        return (time_sq, axial_width_sq, axial_width_sq_err, radial_width_sq, radial_width_sq_err)
+
+    def plot(self, time_sq, width_sq, width_sq_err, type="", param={}):
+        if type == "radial":
+            color = 'C1'
+        elif type == "axial":
+            color = 'C2'
+        else:
+            print("Plot type not supported.")
+            return
+
+        popt, pcov = linearfit(time_sq, width_sq, width_sq_err)
+        fit_chisq = np.sum(((linear(time_sq, *popt)-width_sq)/width_sq_err)**2)
+        reduced_chisq = fit_chisq/(len(time_sq)-2)
+        gof = 100*(1 - stats.chi2.cdf(fit_chisq, len(time_sq)-2)) # in percent, goodness of fit, see https://faculty1.coloradocollege.edu/~sburns/toolbox/DataFitting.html
+        self.ax.errorbar(time_sq, width_sq, yerr=width_sq_err, marker='o', mfc=color, markeredgewidth=0.8, markeredgecolor='k', ecolor=color, linestyle='')
+
+        x = np.linspace(0, np.amax(time_sq), 200)
+        width_sq_fit = linear(x, *popt)
+        c = stats.norm.ppf((1+param["confidence_band"])/2) # 95% confidence level gives critical value c=1.96
+        perr = np.sqrt(np.diag(pcov)) # gives the standard deviation of fitting parameters
+        temp = popt[0]*param["m"]/param["kB"]*1e6 # convert to uK
+        temp_err = c*perr[0]*param["m"]/param["kB"]*1e6
+        radius = np.sqrt(popt[1])
+        radius_err = c*0.5*perr[1]/np.sqrt(popt[1])
+        label = type + ": {:.0f}({:.0f}) uK, {:.2f}({:.2f}) mm, $\chi^2_\\nu$: {:.2f} ({:.0f}%)".format(temp, temp_err, radius, radius_err, reduced_chisq, gof)
+        self.ax.plot(x, width_sq_fit, color, label=label)
+
+        k, b = unc.correlated_values(popt, pcov)
+        py = k*x+b
+        nom = unp.nominal_values(py)
+        std = unp.std_devs(py)
+        self.ax.fill_between(x, nom-c*std, nom+c*std, color=color, alpha=0.2, label="{:.0f}% confidence band".format(param["confidence_band"]*100))
+
+
+filepath = "C:/Users/dur!p5/github/pixelfly-python-control/saved_images/"
+filename = "images_20210526.hdf"
+fname = filepath + filename
+gname = "dcfluormolasses_20210526_171825"
+
+# calculate and plot temperature, inital rms radius, reduced \chi^2, 1-CDF(\chi^2)
+# indicate uncertainties at "confidence_band" confidence level
+# plot pointwise confident band at "confidence_band" level
+tof = tofanalysis(fname, gname)
